7084431bd623ad5a89e63979d992df8dbc7dc3b2
Danila Legkobytov <bjorn-bis@yandex.ru>
2023-01-30 21:29:15 +0700

    feat: Сверстать счётик лайков

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,13 @@ module.exports = {
     static: path.resolve(__dirname, './dist'),
     compress: true,
     port: 8080,
-    open: true
+    open: {
+      app: {
+        name: 'chrome',
+      },
+    },
+    hot: false,
+    liveReload: true,
   },
   module: {
     rules: [
